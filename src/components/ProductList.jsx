1dd4e0f425c5f516e9bbb9c5cba617e79f349a29
Shaun <nhlamulosmashaba@gmail.com>
2024-12-02 16:32:16 +0200

    images

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -173,7 +173,7 @@ const ProductList = () => {
                             <h2>{product.name}</h2>
                             {product.image && (
                                 <img
-                                    src={`${import.meta.env.VITE_API_URL}/products/${product.image}`}
+                                    src={`{product.image}`}
                                     alt={product.name}
                                 />
                             )}
